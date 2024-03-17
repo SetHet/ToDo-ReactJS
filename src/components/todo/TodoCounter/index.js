edc8e2c38f917ff096d9848d65d8c4bc0593b8d9
SetHet <sethet1@gmail.com>
2024-03-17 18:27:36 -0300

    ToDo Counter mejorado

--- src/components/todo/TodoCounter/index.js
+++ src/components/todo/TodoCounter/index.js
@@ -5,11 +5,14 @@ import "./style.css";
 function TodoCounter() {
   const { completedTodos, totalTodos } = React.useContext(TodoContext);
 
-  return (
-    <h1>
-      {completedTodos} de {totalTodos} completados
-    </h1>
-  );
+  const respuesta = () => {
+    if (totalTodos === 0) return "Crea tu primer To Do";
+    if (totalTodos === completedTodos)
+      return `Felicidades has completado los ${completedTodos} To Do`;
+    else return `${completedTodos} de ${totalTodos} completados`;
+  };
+
+  return <h1>{respuesta()}</h1>;
 }
 
 export { TodoCounter };
